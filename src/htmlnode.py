92e2149829050e008415211ae9fa582a7e41bfc0
labhi10.20 <labhi10.20@proton.me>
2025-12-07 15:10:12 -0500

    Created HTMLNode class in htmlnode.py file and unit tests in test_htmlnode.py file

---
+++ src/htmlnode.py
@@ -0,0 +1,20 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError()
+
+    def props_to_html(self):
+        if not self.props:
+            return ''
+        string = ''
+        for key,value in self.props.items():
+            string += f' {key}="{value}"'
+        return string
+
+    def __repr__(self):
+        return f'HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})'
